80cce3f82222e21f926c4eb8b73b9635603518c9
XuXianda <1114186632@qq.com>
2018-03-06 22:05:22 +0800

    ribbon负载均衡

---
+++ microservice-consumer-movie-ribbon/src/main/java/com/xuxianda/ConsumerMovieRibbonApplication.java
@@ -0,0 +1,25 @@
+package com.xuxianda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RibbonClient(name = "microservice-provider-user")
+public class ConsumerMovieRibbonApplication {
+
+  @Bean
+  @LoadBalanced    //负载均衡
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerMovieRibbonApplication.class, args);
+  }
+}
