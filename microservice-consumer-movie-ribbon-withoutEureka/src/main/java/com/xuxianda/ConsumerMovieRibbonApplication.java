2251aa6d8443df650e19ec9bbb893781abc4926c
XuXianda <1114186632@qq.com>
2018-03-11 15:17:39 +0800

    ribbon不使用Eureka

---
+++ microservice-consumer-movie-ribbon-withoutEureka/src/main/java/com/xuxianda/ConsumerMovieRibbonApplication.java
@@ -0,0 +1,27 @@
+package com.xuxianda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RibbonClient(name = "microservice-provider-user", configuration = TestConfiguration.class)
+@ComponentScan(excludeFilters = {@ComponentScan.Filter(value = ExcludeFromComponentScan.class) })
+public class ConsumerMovieRibbonApplication {
+
+  @Bean
+  @LoadBalanced    //负载均衡
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerMovieRibbonApplication.class, args);
+  }
+}
