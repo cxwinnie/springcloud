338c54c151de2c488b8392e0e4f58b01db385fd3
XuXianda <1114186632@qq.com>
2018-03-21 23:51:45 +0800

    config client

--- microservice-config-client/src/main/java/com/xuxianda/ConfigClientApplication.java
+++ microservice-config-client/src/main/java/com/xuxianda/ConfigClientApplication.java
@@ -1,18 +1,16 @@
-package com.xuxianda.eureka;
+package com.xuxianda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by Xianda Xu on 2018/03/01 14:46.
  */
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class,args);
+        SpringApplication.run(ConfigClientApplication.class,args);
     }
 
 }
